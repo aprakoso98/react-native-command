592def7f6a9a760ecd3273abef1cee15fe705af1
aprakoso98 <adhyt.scott@gmail.com>
2021-11-27 23:03:33 +0700

    can move aab

--- src/moveApp.ts
+++ src/moveApp.ts
@@ -3,17 +3,23 @@ import * as moment from "moment"
 import { theParams } from "../bin"
 const ANDROID_PATH = "android"
 const apkPath = `./${ANDROID_PATH}/app/build/outputs/apk/release/app-release.apk`
+const aabPath = `./${ANDROID_PATH}/app/build/outputs/bundle/release/app.aab`
 const outputFolder = "outputs"
 
 function moveApp() {
 	const { name: projectName }: { name: string } = require(`${process.env.PWD}/package.json`)
-	type Params = '--filename' | '-f'
+	type Params = '--filename' | '-f' | 'aab'
 	const {
-		'--filename': _filename, '-f': __filename
+		'--filename': _filename, '-f': __filename,
 	} = theParams as MyObject<Params>
-	const filename = _filename ?? __filename ?? `${projectName} ${moment().format('YYYY-MM-DD HH-mm-ss')}.apk`
-	console.log(filename)
-	exec(`cp "${apkPath}" "./${outputFolder}/${filename}"`, (err) => {
+	let filename = `${projectName}-Bundle-${moment().format('YYYY-MM-DD-HH-mm-ss')}.aab`
+	let pathFile = apkPath
+	if ('aab' in theParams) {
+		pathFile = aabPath
+	} else {
+		filename = _filename ?? __filename ?? `${projectName}-${moment().format('YYYY-MM-DD-HH-mm-ss')}.apk`
+	}
+	exec(`cp "${pathFile}" "./${outputFolder}/${filename}"`, (err) => {
 		if (err) { console.error(err); return }
 		console.log(`${filename} copied`)
 	})
