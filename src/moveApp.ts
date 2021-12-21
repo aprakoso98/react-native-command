ab1b98f50b4e0e56736ab2a07fe1c026ff3c8f8d
aprakoso98 <adhyt.scott@gmail.com>
2021-12-21 13:23:36 +0700

    moveApp

--- src/moveApp.ts
+++ src/moveApp.ts
@@ -2,16 +2,20 @@ import { exec } from "child_process"
 import * as moment from "moment"
 import { theParams } from "../bin"
 const ANDROID_PATH = "android"
-const apkPath = `./${ANDROID_PATH}/app/build/outputs/apk/release/app-release.apk`
-const aabPath = `./${ANDROID_PATH}/app/build/outputs/bundle/release/app.aab`
 const outputFolder = "outputs"
 
 function moveApp() {
 	const { name: projectName }: { name: string } = require(`${process.env.PWD}/package.json`)
-	type Params = '--filename' | '-f' | 'aab'
+	type Params = '--source' | '-s' | '--filename' | '-f' | 'aab'
 	const {
 		'--filename': _filename, '-f': __filename,
+		'--source': _source, '-s': __source
 	} = theParams as MyObject<Params>
+
+	const source = _source ?? __source
+	const apkPath = `./${ANDROID_PATH}/app/build/outputs/apk${source ?? '/release/app-release.apk'}`
+	const aabPath = `./${ANDROID_PATH}/app/build/outputs/bundle${source ?? '/release/app.aab'}`
+	
 	let filename = `${projectName}-Bundle-${moment().format('YYYY-MM-DD-HH-mm-ss')}.aab`
 	let pathFile = apkPath
 	if ('aab' in theParams) {
